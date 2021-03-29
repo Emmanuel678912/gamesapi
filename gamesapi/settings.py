46cd35723003b103b0f785a9ff39e3d7d7ed3736
Emmanuel <monotone_briggs@hotmail.com>
2021-03-29 14:38:23 +0000

    settings

--- gamesapi/settings.py
+++ gamesapi/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_GAMESAPI')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://emmanuelgamesapi.herokuapp.com/']
 
 
 # Allows program to be run when DEBUG = False
